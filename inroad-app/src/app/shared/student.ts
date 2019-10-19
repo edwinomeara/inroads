b24550ce9fd2aefc400c2111009a4c1512a48937
Edwin O'Meara <EdwinHOmeara@gmail.com>
2019-10-19 04:28:40 -0400

    student progress bar updates

---
+++ inroad-app/src/app/shared/student.ts
@@ -0,0 +1,14 @@
+export class User {
+  public username: string;
+  public password: string;
+  public status: string;
+  public firstName: string;
+  public lastName: string;
+  public age: string;
+  public grade: string;
+  public major: string;
+  public jobInterestType: string;
+  public school: string;
+  public locationcity: string;
+  public locationstate: string;
+}
--- inroad-app/src/app/student-page/student-page.component.ts
+++ inroad-app/src/app/student-page/student-page.component.ts
@@ -6,12 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-page.component.css']
 })
 export class StudentPageComponent implements OnInit {
-  resumeSubmit = true;
+  resumeSubmit = false;
   training = false;
   interview = false;
   resumeApproval = false;
+  isNew = true;
+
+  fileToUpload: File = null;
 
   constructor() {}
 
   ngOnInit() {}
+
+  onResumeSubmit() {
+    this.resumeSubmit = true;
+    this.isNew = false;
+  }
+
+  onTraining() {
+    this.training = true;
+    this.resumeSubmit = false;
+  }
+
+  onMock() {
+    this.interview = true;
+    this.resumeSubmit = false;
+  }
+
+  onApproval() {
+    this.interview = false;
+    this.resumeApproval = true;
+  }
 }
