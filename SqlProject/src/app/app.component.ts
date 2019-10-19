3310e377465957bbffcc3b715f05e598e010b2fb
Rohan <rohan.guliani285460@gmail.com>
2019-10-19 07:54:46 -0400

    updating sql code

--- SqlProject/src/app/app.component.ts
+++ SqlProject/src/app/app.component.ts
@@ -1,7 +1,49 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
+var Sqlite = require("nativescript-sqlite");
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "./app.component.html"
+    selector: "my-app",
+    templateUrl: "app.component.html",
 })
-export class AppComponent { }
+export class AppComponent {
+
+    private database: any;
+    public people: Array<any>;
+
+    public constructor() {
+        this.people = [];
+        (new Sqlite("my.db")).then(db => {
+            db.execSQL("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)").then(id => {
+                this.database = db;
+            }, error => {
+                console.log("CREATE TABLE ERROR", error);
+            });
+        }, error => {
+            console.log("OPEN DB ERROR", error);
+        });
+    }
+
+    public insert() {
+        this.database.execSQL("INSERT INTO people (firstname, lastname) VALUES (?, ?)", ["Nic", "Raboy"]).then(id => {
+            console.log("INSERT RESULT", id);
+            this.fetch();
+        }, error => {
+            console.log("INSERT ERROR", error);
+        });
+    }
+
+    public fetch() {
+        this.database.all("SELECT * FROM people").then(rows => {
+            this.people = [];
+            for(var row in rows) {
+                this.people.push({
+                    "firstname": rows[row][1],
+                    "lastname": rows[row][2]
+                });
+            }
+        }, error => {
+            console.log("SELECT ERROR", error);
+        });
+    }
+
+}
