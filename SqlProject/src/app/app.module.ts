75d53e50f14b3eefe620b8b12f15bc0bfaff7508
Rohan <rohan.guliani285460@gmail.com>
2019-10-19 11:47:45 -0400

    addingComments

--- SqlProject/src/app/app.module.ts
+++ SqlProject/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+// main backend file
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
