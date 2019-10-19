5752188ec1bcefc97b466928401cfbbc7678b2ce
Rohan <rohan.guliani285460@gmail.com>
2019-10-19 02:06:50 -0400

    updated backend code

--- backend/testDB.js
+++ backend/testDB.js
@@ -1,16 +1,26 @@
 const sqlite3 = require('sqlite3').verbose();
  
-let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the chinook database.');
-  });
+// open the database
+let db = new sqlite3.Database('./chinook/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  else console.log('Connected to the chinook database.');
+});
+ 
+let sql = `SELECT FirstName firstName,
+                  LastName lastName,
+                  Email email
+            FROM customers
+            WHERE Country = ?
+            ORDER BY FirstName`;
+ 
+db.each(sql, ['USA'], (err, row) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`${row.firstName} ${row.lastName} - ${row.email}`);
+});
  
 // close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+db.close();
