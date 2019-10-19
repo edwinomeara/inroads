3cc5b7728b9fcd529cc7a5875cc4d737361e22c4
SydneyLee <32982533+SydneyLe@users.noreply.github.com>
2019-10-19 03:38:07 -0400

    complete registration

--- inroad-app/src/app/company-registration/company-registration.component.ts
+++ inroad-app/src/app/company-registration/company-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user';
 
 @Component({
   selector: 'app-company-registration',
@@ -9,6 +10,50 @@ export class CompanyRegistrationComponent implements OnInit {
 
   constructor() { }
 
+  userName = '';
+  passWord = '';
+  cname = '';
+  grade = '';
+  major = '';
+
+  duplicated = false;
+  isEmpty = false;
+
+  remove = '';
+
+  user = new User('', '');
+
+  submitted = false;
+
+  onSubmit() {
+    if (this.userName == '') {
+      this.isEmpty = true;
+      return;
+    }
+
+    if (this.passWord == '') {
+      this.isEmpty = true;
+      return;
+    }
+
+    if (this.cname == '') {
+      this.isEmpty = true;
+      return;
+    }
+
+    if (this.grade == '') {
+      this.isEmpty = true;
+      return;
+    }
+
+    if (this.major == '') {
+      this.isEmpty = true;
+      return;
+    }
+
+    this.submitted = true;
+    console.log(JSON.stringify(this.user)); // Here's the data in json
+  }
   ngOnInit() {
   }
 
